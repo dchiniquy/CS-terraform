7af142a89625d174be81f19f4012c5adf1319dbd
Don Chiniquy <drchiniquy@gmail.com>
2025-06-11 12:29:09 -0700

    Moved variables to the correct place

--- environments/production/variables.tf
+++ environments/production/variables.tf
@@ -2,3 +2,18 @@ variable "credentials_file" {
   description = "Path to the Google Cloud credentials file"
   type        = string
 }
+
+variable "gcs_bucket_name" {
+  description = "Name of the GCS bucket for Terraform state."
+  type        = string
+}
+
+variable "gcs_prefix" {
+  description = "Prefix for the GCS bucket."
+  type        = string
+}
+
+variable "gcs_project_id" {
+  description = "Project ID for the GCS bucket."
+  type        = string
+}
